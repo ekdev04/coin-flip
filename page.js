7b0367c0b0f76f3b4d3c55cde6d2d599a8af02c7
ekdev04 <ekd04@iastate.edu>
2025-04-15 15:05:33 -0500

    wrote css and js

--- page.js
+++ page.js
@@ -1,5 +1,17 @@
-function run() {
-    let test= document.getElementById('test')
-
-    test.innerHTML= "Hello, World!";
+function runGame(x) {
+    let choiceresult= x
+    let randNum=Math.floor(Math.random() *2) +1;
+    let flipResult;
+        if (randNum ===1){
+            flipResult='tails';
+        }  else{
+            flipResult='heads';
+        };
+    let gameResult;
+        if (flipResult === choiceresult){
+            gameResult= "win";
+        } else{
+            gameResult="loss";
+        };
+        
 }
